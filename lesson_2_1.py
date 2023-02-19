7be2d7b666230516b521b43e1019eeeb795d1594
PavelApanas <apanaspavel@gmail.com>
2023-02-19 20:30:29 +0300

    Lesson_2_HomeWork

--- lesson_2_1.py
+++ lesson_2_1.py
@@ -3,6 +3,14 @@
 #Пользователь вводит предложение, заменить все пробелы на "-" 2-мя
 #способами
 
+#variant_1
 s = input("Введите предложение: ")
 result = "-".join(s.split())
+print(result)
+
+
+#variant_2
+
+s = input("Введите предложение: ")
+result = s.replace(" ", "-")
 print(result)
