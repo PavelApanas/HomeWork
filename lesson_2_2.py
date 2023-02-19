d972db17c3b5415e05b572698f384776bd6d16d6
PavelApanas <apanaspavel@gmail.com>
2023-02-19 19:52:43 +0300

    Lesson_2_HomeWork

--- lesson_2_2.py
+++ lesson_2_2.py
@@ -1,3 +1,6 @@
+#2.2
+# Пользователь вводит 3 числа, найти среднее арифмитическое с точность 3
+
 a = float(input("Insert number 1 "))
 b = float(input("Insert number 2: "))
 c = float(input("Insert number 3 "))
