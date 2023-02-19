2117a163177e6400141812e2afa259540aedd743
PavelApanas <apanaspavel@gmail.com>
2023-02-19 20:46:32 +0300

    Lesson_2_HomeWork

--- lesson_2_4.py
+++ lesson_2_4.py
@@ -1,4 +1,3 @@
-
 #2.4
 #Пользователь вводит 3 числа, сказать сколько из них положительных
 #и сколько отрицательных
@@ -11,23 +10,6 @@ num3 = float(input("Введите 3 число: "))
 pos = (num1 > 0) + (num2 > 0) + (num3 > 0)
 neg = (num1 < 0) + (num2 < 0) + (num3 < 0)
 
-# pos = 0
-# neg = 0
-#
-# if num1 > 0:
-#     pos += 1
-# elif num1 < 0:
-#     neg += 1
-#
-# if num2 > 0:
-#     pos += 1
-# elif num2 < 0:
-#     neg += 1
-#
-# if num3 > 0:
-#     pos += 1
-# elif num3 < 0:
-#     neg += 1
 
 print("Sum_positive:", pos)
 print("Sum_negative:", neg)
