d965a9920534635cef7bbc1cbd5e4899a3765399
Pavel Apanas <apanaspavel@gmail.com>
2023-02-26 21:17:06 +0300

    Homework

--- lesson_5_4.py
+++ lesson_5_4.py
@@ -1,2 +1,7 @@
 def filter_strings(lst):
     return filter(lambda x: isinstance(x, str), lst)
+
+lst = [1, 'hello', 3.14, True, 'world']
+filtered_lst = filter_strings(lst)
+for s in filtered_lst:
+        print(s)
