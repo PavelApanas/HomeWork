6d6f9ac1e26c9824a2638b07948e78de8780f06c
Pavel Apanas <apanaspavel@gmail.com>
2023-03-12 19:29:55 +0300

    lesson_bd

---
+++ lesson_bd_1.py
@@ -0,0 +1,47 @@
+import sqlalchemy
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Status(Base):
+    __tablename__ = 'statuses'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(10), unique=True)
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(24))
+    email = Column(String(24), unique=True)
+    orders = relationship("Order", backref="user")
+
+class Order(Base):
+    __tablename__ = 'orders'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    status_id = Column(Integer, ForeignKey('statuses.id'))
+    status = relationship("Status")
+    order_items = relationship("OrderItem", backref="order")
+
+class Product(Base):
+    __tablename__ = 'products'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(36))
+    description = Column(String(140))
+    category_id = Column(Integer, ForeignKey('categories.id'))
+    category = relationship("Category")
+
+class Category(Base):
+    __tablename__ = 'categories'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(24), unique=True)
+    products = relationship("Product", backref="category")
+
+class OrderItem(Base):
+    __tablename__ = 'order_items'
+    id = Column(Integer, primary_key=True)
+    order_id = Column(Integer, ForeignKey('orders.id'))
+    product_id = Column(Integer, ForeignKey('products.id'))
+    product = relationship("Product")
